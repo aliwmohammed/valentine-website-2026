a8df43952fb46fedb05951f1bc2f905127824d9b
aliwmohammed <131821059+aliwmohammed@users.noreply.github.com>
2026-02-03 22:14:08 -0500

    Update  006

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=SOJpE1KMUbo&list=RDSOJpE1KMUbo&start_radio=1, // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dycu0ptma/video/upload/v1770174612/Dave_-_Raindance__ft._Tems__128k_o58gej.mp3"
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
